97935bce155b91b5a28a5534d464e4de21949933
aryan9721 <avora9721@gmail.com>
2023-11-03 19:19:58 +0530

    api changed

--- FRONTEND/src/pages/api/index.js
+++ FRONTEND/src/pages/api/index.js
@@ -1,5 +1,5 @@
 // const baseURL = 'https://parking-management-service-804561330.development.catalystserverless.com/server/parking-management-service/';
-const baseURL = 'http://localhost:5000/';
+const baseURL = 'http://192.46.210.164:5000/';
 // const baseURL = '/server/parking-management-service/';
 // const baseURL = 'http://parking-clip.com/server/parking-management-service/';
 // const baseURL = 'http://192.168.0.103:5001/';
